4ebb67f7549617d42735d23abea38cd1314a2d8d
JoseDaniel <jduvmx@gmail.com>
2023-05-19 21:49:33 -0600

    Changed the post navigation layout

---
+++ content.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying posts within the loop.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata( 'article' ); ?>>
+	<div class="inside-article custom-individual-post">
+		<?php
+		/**
+		 * generate_before_content hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_featured_page_header_inside_single - 10
+		 */
+		do_action( 'generate_before_content' );
+
+		/**
+		 * generate_after_entry_header hook.
+		 *
+		 * @since 0.1
+		 *
+		 * @hooked generate_post_image - 10
+		 */
+		do_action( 'generate_after_entry_header' );
+
+		$itemprop = '';
+
+		if ( 'microdata' === generate_get_schema_type() ) {
+			$itemprop = ' itemprop="text"';
+		}
+
+		if ( generate_show_excerpt() ) :
+			?>
+
+			<?php
+				/**
+				 * generate_before_entry_title hook.
+				 *
+				 * @since 0.1
+				 */
+				do_action( 'generate_before_entry_title' );
+
+				if ( generate_show_title() ) {
+					$params = generate_get_the_title_parameters();
+
+					the_title( $params['before'], $params['after'] );
+				}
+			?>
+
+		<?php else : ?>
+
+			<div class="entry-content"<?php echo $itemprop; // phpcs:ignore -- No escaping needed. ?>>
+				<?php
+				the_content();
+
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
+						'after'  => '</div>',
+					)
+				);
+				?>
+			</div>
+
+			<?php
+		endif;
+
+		/**
+		 * generate_after_content hook.
+		 *
+		 * @since 0.1
+		 */
+		do_action( 'generate_after_content' );
+		?>
+	</div>
+</article>
