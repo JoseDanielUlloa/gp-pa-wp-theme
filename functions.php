d16a7169747c8b66dccc89b5f80594b2a97b0b94
JoseDaniel <jduvmx@gmail.com>
2023-05-23 23:39:20 -0600

    Added FontAwesome

--- functions.php
+++ functions.php
@@ -12,8 +12,10 @@
 
 // Enqueue scripts and styles
 function generatepress_child_scripts(){
+    $version = wp_get_theme()->get( 'Version' );
 	wp_enqueue_style( 'generatepress-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'generatepress-style' ));
+    wp_enqueue_style( 'fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css", array(), '6.4.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'generatepress_child_scripts' );
 
